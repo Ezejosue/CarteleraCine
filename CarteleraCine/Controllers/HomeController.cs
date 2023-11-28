b343d127ef8e74c872651704c73041ff21849eeb
Gerardo Marroquin <91712475+g00004@users.noreply.github.com>
2023-11-28 00:12:05 -0600

    Varios cambios frontend
    Estructura Base Web,
    Peliculas Index,
    Detalle pelicula,
    Administación Crear pelicula

--- CarteleraCine/Controllers/HomeController.cs
+++ CarteleraCine/Controllers/HomeController.cs
@@ -1,21 +1,46 @@
 ﻿using CarteleraCine.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using CarteleraCine.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarteleraCine.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CinePlusContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CinePlusContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return _context.Peliculas != null ?
+                         View(await _context.Peliculas.ToListAsync()) :
+                         Problem("Entity set 'CinePlusContext.Peliculas'  is null.");
+        }
+
+
+        // GET: Home/Detalle/5
+        public async Task<IActionResult> Detalle(int? id)
+        {
+            if (id == null || _context.Peliculas == null)
+            {
+                return NotFound();
+            }
+
+            var pelicula = await _context.Peliculas
+                .FirstOrDefaultAsync(m => m.PeliculaId == id);
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+
+            return View(pelicula);
         }
 
         public IActionResult Privacy()
