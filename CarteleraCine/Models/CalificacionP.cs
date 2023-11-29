50f1089b510a8995d4736a252f795b67d677930d
Josué Ávalos <aezequiel56@gmail.com>
2023-11-29 11:42:14 -0600

    Update y create de calificaciones corregido

---
+++ CarteleraCine/Models/CalificacionP.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CarteleraCine.Models
+{
+    public class CalificacionP
+    {
+        public Calificacione oCalificacion { get; set; }
+
+        public List<SelectListItem> oIdPelicula { get; set; }
+    }
+}
