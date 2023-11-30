3b35a4af461e34bbc36c7697477b36336e4598d6
Josué Ávalos <aezequiel56@gmail.com>
2023-11-30 14:33:34 -0600

    QueryUpdate

--- Util/Cartelara.sql
+++ Util/Cartelara.sql
@@ -90,11 +90,17 @@ GO
 
 INSERT INTO Peliculas (Titulo, Sinopsis, Director, Genero, Clasificacion, Duracion, TipoButacas, Poster)
 VALUES
-    ('El Padrino', 'Un poderoso jefe de la mafia busca mantener el control de su imperio criminal.', 'Francis Ford Coppola', 'Drama', 'R', 175, 'VIP', '/uploads/John.jpg'),
-    ('Titanic', 'Un romance épico entre pasajeros a bordo del famoso barco que se hunde.', 'James Cameron', 'Romance', 'PG-13', 195, 'Estándar', '/uploads/John.jpg'),
-    ('Star Wars: Episodio IV', 'Un joven granjero se convierte en un héroe galáctico en una galaxia lejana.', 'George Lucas', 'Ciencia ficción', 'PG', 121, 'IMAX', '/uploads/John.jpg'),
-    ('Jurassic Park', 'Un parque de dinosaurios genéticamente recreados se sale de control.', 'Steven Spielberg', 'Aventura', 'PG-13', 127, 'Estándar', '/uploads/John.jpg'),
-    ('Inception', 'Un ladrón de sueños entra en la mente de las personas para robar secretos.', 'Christopher Nolan', 'Ciencia ficción', 'PG-13', 148, 'VIP', '/uploads/John.jpg');
+
+	('Avatar', 'Un marine en un mundo alienígena se involucra en un conflicto entre humanos y nativos.', 'James Cameron', 'Ciencia ficción', 'PG-13', 162, 'Estándar', '/uploads/Avatar.jpg'),
+    ('The Shawshank Redemption', 'Un hombre inocente encuentra la redención en una prisión de máxima seguridad.', 'Frank Darabont', 'Drama', 'R', 142, 'VIP', '/uploads/ShawshankRedemption.jpg'),
+    ('The Dark Knight', 'Batman se enfrenta a su enemigo más siniestro, el Joker.', 'Christopher Nolan', 'Acción', 'PG-13', 152, 'Estándar', '/uploads/DarkKnight.jpg'),
+    ('Forrest Gump', 'Un hombre con discapacidad intelectual vive una vida extraordinaria.', 'Robert Zemeckis', 'Drama', 'PG-13', 142, 'VIP', '/uploads/ForrestGump.jpg'),
+    ('Interstellar', 'Un grupo de exploradores viaja a través de un agujero de gusano en busca de un nuevo hogar para la humanidad.', 'Christopher Nolan', 'Ciencia ficción', 'PG-13', 169, 'IMAX', '/uploads/Interstellar.jpg'),
+    ('The Matrix', 'Un hacker descubre que vive en un mundo simulado y se une a la resistencia contra las máquinas.', 'The Wachowskis', 'Ciencia ficción', 'R', 136, 'Estándar', '/uploads/Matrix.jpg'),
+    ('The Godfather Part II', 'La saga de una familia mafiosa continúa mientras el joven Vito Corleone se convierte en un poderoso líder.', 'Francis Ford Coppola', 'Drama', 'R', 202, 'Estándar', '/uploads/GodfatherPartII.jpg'),
+    ('Schindler''s List', 'Un empresario alemán salva a un grupo de judíos durante el Holocausto.', 'Steven Spielberg', 'Drama', 'R', 195, 'VIP', '/uploads/SchindlersList.jpg'),
+    ('Pulp Fiction', 'Varios personajes se entrelazan en una trama de crimen y redención.', 'Quentin Tarantino', 'Crimen', 'R', 154, 'Estándar', '/uploads/PulpFiction.jpg');
+
 GO
 
 INSERT INTO Calificaciones (PeliculaID, Puntuacion, FechaCalificacion)
@@ -104,8 +110,16 @@ VALUES
     (6, 3, GETDATE()),
     (7, 4, GETDATE()),
     (8, 5, GETDATE()),
-    (9, 3, GETDATE());
-	
+    (9, 3, GETDATE()),
+	(10, 4, GETDATE()),
+    (11, 5, GETDATE()),
+    (12, 3, GETDATE()),
+    (13, 4, GETDATE()),
+    (14, 5, GETDATE()),
+    (15, 3, GETDATE()),
+	(16, 4, GETDATE()),
+    (17, 5, GETDATE()),
+    (18, 3, GETDATE());
 GO
 
 
@@ -116,7 +130,16 @@ VALUES
     (6, 1, '2023-12-01 20:00:00'), -- Ejemplo de horario para Película 6
     (7, 1, '2023-12-02 14:00:00'), -- Ejemplo de horario para Película 7
     (8, 1, '2023-12-02 16:30:00'), -- Ejemplo de horario para Película 8
-    (9, 1, '2023-12-02 19:00:00');
+    (9, 1, '2023-12-02 19:00:00'),
+	(10, 1, '2023-12-03 16:00:00'),
+    (11, 1, '2023-12-03 18:30:00'),
+    (12, 1, '2023-12-03 21:00:00'),
+    (13, 1, '2023-12-04 15:00:00'),
+    (14, 1, '2023-12-04 17:30:00'),
+    (15, 1, '2023-12-04 20:00:00'),
+	(16, 1, '2023-12-05 16:00:00'),
+    (17, 1, '2023-12-05 18:30:00'),
+    (18, 1, '2023-12-05 21:00:00');
 GO
 
 
